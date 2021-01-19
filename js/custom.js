510f53357207f1df698441a0bff00d4005acd849
Ulwan Isma <ahmadulwann@gmail.com>
2021-01-19 18:52:35 +0800

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -7,22 +7,46 @@
 $(window).load(function(){
     $('.preloader').fadeOut(1000); // set duration in brackets
 
+
+  /*-------------------------------------------------------------------------------
+    MODAL IMAGE PORTFOLIO
+  -------------------------------------------------------------------------------*/
+
     // Get the modal
     var modal = document.getElementById("myModal");
+    var modal2 = document.getElementById("myModal2");
      
     // Get the image and insert it inside the modal - use its "alt" text as a caption
     var img1 = document.getElementById("myImg");
+    var img2 = document.getElementById("myImg2");
+    var img3 = document.getElementById("myImg3");
+    var img4 = document.getElementById("myImg4");
+    var img5 = document.getElementById("myImg5");
+    var img6 = document.getElementById("myImg6");
+
     var modalImg = document.getElementById("img01");
-    var captionText1 = document.getElementById("caption");
+    var modalImg2 = document.getElementById("img02");
+    var modalImg3 = document.getElementById("img03");
+    var modalImg4 = document.getElementById("img04");
+    var modalImg5 = document.getElementById("img05");
+    var modalImg6 = document.getElementById("img06");
+
+    //var captionText1 = document.getElementById("caption");
 
     img1.onclick = function(){
       modal.style.display = "block";
       modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
+      //captionText1.innerHTML = this.alt;
+    }
+
+    img2.onclick = function(){
+      modal2.style.display = "block";
+      modalImg2.src = this.src;
+      //captionText1.innerHTML = this.alt;
     }
     
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    var span = document.getElementById("closeBtn");
     
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() { 
@@ -31,7 +55,7 @@ $(window).load(function(){
     
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-      if (event.target == modal) {
+      if (event.target == '') {
         $('.modal').fadeOut(400);
       }
     }
@@ -91,4 +115,13 @@ $(window).load(function(){
     
     });
   });
+
+  /*-------------------------------------------------------------------------------
+    Light Gallery
+  -------------------------------------------------------------------------------*/
+
+
+  lightGallery(document.getElementById('lightgallery'));
+
+
 });
